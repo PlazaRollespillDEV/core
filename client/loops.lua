b9b434b287e68ff35558b0993706ad2898942bbe
Greve <141843261+PlazaRollespillDEV@users.noreply.github.com>
2024-04-28 06:19:54 +0200

    Update loops.lua

--- client/loops.lua
+++ client/loops.lua
@@ -2,8 +2,8 @@ CreateThread(function()
     while true do
         local sleep = 0
         if LocalPlayer.state.isLoggedIn then
-            sleep = (1000 * 60) * QBCore.Config.UpdateInterval
-            TriggerServerEvent('QBCore:UpdatePlayer')
+            sleep = (1000 * 60) * FW.Config.UpdateInterval
+            TriggerServerEvent('Framework:UpdatePlayer')
         end
         Wait(sleep)
     end
@@ -12,13 +12,13 @@ end)
 CreateThread(function()
     while true do
         if LocalPlayer.state.isLoggedIn then
-            if (QBCore.PlayerData.metadata['hunger'] <= 0 or QBCore.PlayerData.metadata['thirst'] <= 0) and not (QBCore.PlayerData.metadata['isdead'] or QBCore.PlayerData.metadata['inlaststand']) then
+            if (FW.PlayerData.metadata['hunger'] <= 0 or FW.PlayerData.metadata['thirst'] <= 0) and not (FW.PlayerData.metadata['isdead'] or FW.PlayerData.metadata['inlaststand']) then
                 local ped = PlayerPedId()
                 local currentHealth = GetEntityHealth(ped)
                 local decreaseThreshold = math.random(5, 10)
                 SetEntityHealth(ped, currentHealth - decreaseThreshold)
             end
         end
-        Wait(QBCore.Config.StatusInterval)
+        Wait(FW.Config.StatusInterval)
     end
 end)
